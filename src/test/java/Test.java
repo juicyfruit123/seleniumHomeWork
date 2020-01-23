b2e48c3eaaaf3d918a817c1c96ed98825eb4448a
anton <lumencoeli48@gmail.com>
2020-01-23 20:26:03 +0300

    33

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -13,14 +13,14 @@ import java.util.concurrent.TimeUnit;
 
 public class Test {
     private static WebDriver webDriver = null;
-    private static final List<String> ALL_SITES = Arrays.asList("http://twitter.com/sberbank", "https://www.facebook.com/bankdruzey", "http://www.youtube.com/sberbank", "http://instagram.com/sberbank"
+    private static final List<String> ALL_SITES = Arrays.asList("https://www.facebook.com/bankdruzey", "http://twitter.com/sberbank", "http://www.youtube.com/sberbank", "http://instagram.com/sberbank"
             , "http://vk.com/sberbank", "https://ok.ru/sberbank");
     private static JavascriptExecutor JAVASCRIPT_EXECUTOR = null;
 
     @BeforeClass
     public static void testUP() {
 
-        System.setProperty("webdriver.chrome.driver", "/home/anton/Загрузки/seleniumHomeWork/drivers/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
 
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
@@ -37,17 +37,15 @@ public class Test {
     @org.junit.Test
     public void rGS() {
         webDriver.get("http://sberbank.ru/ru/person");
-        WebElement currentRegion = webDriver.findElement(By.xpath("//a[@class='hd-ft-region']"));
-        currentRegion.click();
+        webDriver.findElement(By.xpath("//a[@class='hd-ft-region']")).click();
         webDriver.findElement(By.xpath("//input[@placeholder='Москва']")).sendKeys("Нижегородская область");
         webDriver.findElement(By.xpath("//a[contains(text(),'Нижегородская')]")).click();
-        currentRegion = webDriver.findElement(By.xpath("//a[@class='hd-ft-region']"));
-        Assert.assertTrue(currentRegion.getAttribute("aria-label").contains("Нижегородская область"));
+        WebElement currentRegion = webDriver.findElement(By.xpath("//div[@class='hd-ft-region__title']/span"));
+        Assert.assertEquals("Неверно отобразилось местоположение на странице", currentRegion.getText(), "Нижегородская область");
         List<WebElement> iconFooterSocial = webDriver.findElements(By.xpath("//li[@class='footer__social_item']/a"));
         JAVASCRIPT_EXECUTOR.executeScript("return arguments[0].scrollIntoView(true);", iconFooterSocial.get(1));
-        for (WebElement webElement : iconFooterSocial) {
-            String href = webElement.getAttribute("href");
-            Assert.assertTrue(ALL_SITES.contains(href));
+        for (int i = 0; i < iconFooterSocial.size(); i++) {
+            Assert.assertEquals("Неверно отобразилось местоположение на странице", ALL_SITES.get(i), iconFooterSocial.get(i).getAttribute("href"));
         }
     }
 
