185aa7ea6caa18fd4c2b024e1caa8e41751f5473
anton <lumencoeli48@gmail.com>
2020-01-23 13:42:34 +0300

    22

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -12,7 +12,10 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Test {
-    static WebDriver webDriver = null;
+    private static WebDriver webDriver = null;
+    private static final List<String> ALL_SITES = Arrays.asList("http://twitter.com/sberbank", "https://www.facebook.com/bankdruzey", "http://www.youtube.com/sberbank", "http://instagram.com/sberbank"
+            , "http://vk.com/sberbank", "https://ok.ru/sberbank");
+    private static JavascriptExecutor JAVASCRIPT_EXECUTOR = null;
 
     @BeforeClass
     public static void testUP() {
@@ -22,6 +25,7 @@ public class Test {
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
+        JAVASCRIPT_EXECUTOR = (JavascriptExecutor) webDriver;
     }
 
     @AfterClass
@@ -33,20 +37,17 @@ public class Test {
     @org.junit.Test
     public void rGS() {
         webDriver.get("http://sberbank.ru/ru/person");
-        WebElement element = webDriver.findElement(By.xpath("//a[@class='hd-ft-region']"));
-        element.click();
+        WebElement currentRegion = webDriver.findElement(By.xpath("//a[@class='hd-ft-region']"));
+        currentRegion.click();
         webDriver.findElement(By.xpath("//input[@placeholder='Москва']")).sendKeys("Нижегородская область");
         webDriver.findElement(By.xpath("//a[contains(text(),'Нижегородская')]")).click();
-        WebElement element1 = webDriver.findElement(By.xpath("//a[@class='hd-ft-region']"));
-        Assert.assertTrue(element1.getAttribute("aria-label").contains("Нижегородская область"));
-        List<WebElement> list = webDriver.findElements(By.xpath("//li[@class='footer__social_item']/a"));
-        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
-        javascriptExecutor.executeScript("return arguments[0].scrollIntoView(true);", list.get(0));
-        List<String> ar = Arrays.asList("http://twitter.com/sberbank", "https://www.facebook.com/bankdruzey", "http://www.youtube.com/sberbank", "http://instagram.com/sberbank"
-                , "http://vk.com/sberbank", "https://ok.ru/sberbank");
-        for (WebElement webElement : list) {
+        currentRegion = webDriver.findElement(By.xpath("//a[@class='hd-ft-region']"));
+        Assert.assertTrue(currentRegion.getAttribute("aria-label").contains("Нижегородская область"));
+        List<WebElement> iconFooterSocial = webDriver.findElements(By.xpath("//li[@class='footer__social_item']/a"));
+        JAVASCRIPT_EXECUTOR.executeScript("return arguments[0].scrollIntoView(true);", iconFooterSocial.get(1));
+        for (WebElement webElement : iconFooterSocial) {
             String href = webElement.getAttribute("href");
-            Assert.assertTrue(ar.contains(href));
+            Assert.assertTrue(ALL_SITES.contains(href));
         }
     }
 
